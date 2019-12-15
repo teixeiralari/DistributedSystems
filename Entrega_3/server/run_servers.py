5a58c7cd90357cc04d54c6911e7e20a2f875d741
Larissa Teixeira <rteixeira.larissa@gmail.com>
2019-12-15 10:24:53 -0300

    update

--- Entrega_3/server/run_servers.py
+++ Entrega_3/server/run_servers.py
@@ -2,24 +2,30 @@
 import os
 import sys 
 
+
+PORT = 50050
 try:
-    PORT = 50050
-    try:
-            N_nodes = int(sys.argv[1])
-            N_replicas = int(sys.argv[2])
-    except:
-            N_nodes = 3
-            N_replicas = 3
-            
-    print(f"Total Servers {N_nodes * N_replicas}")
-    for i in range(N_nodes * N_replicas):
-            print(f"PORT: {PORT}")
-            build_x = f'docker build -t neonat_{i} .'
-            run_x = f'docker run -d -p {PORT}:{PORT} -e "SERVER_PORT={PORT}" --network="host" neonat_{i}'
-            print(f"Running: {build_x}")
-            os.system(build_x)
-            print(f"Running: {run_x}")
-            os.system(run_x)
-            PORT += 1
-except Exception as e:
-    print(f'{e}')
+        N_nodes = int(sys.argv[1])
+        N_replicas = int(sys.argv[2])
+except:
+        N_nodes = 3
+        N_replicas = 3
+        
+print(f"Number of servers: {N_nodes}\nNumber of replicas: {N_replicas}\nTotal of servers running: {N_nodes * N_replicas}\n")
+print('Cleaning all containers')
+os.system('docker rm -f $(docker ps -a -q)')
+
+for i in range(N_nodes * N_replicas):
+        #Command to build container
+        build_neonat_container = f'docker build -t neonat_{i} .'
+
+        #Command to run container
+        run_neonat_container = f'docker run -d -p {PORT}:{PORT} -e "SERVER_PORT={PORT}" --network="host" neonat_{i}'
+        print(f"Running: {build_neonat_container}")
+        #Exec command to build container
+        os.system(build_neonat_container)
+
+        print(f"Running: {run_neonat_container}")
+        #Exec command to run container
+        os.system(run_neonat_container)
+        PORT += 1
